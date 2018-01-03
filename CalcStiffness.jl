dc12758e688a3aa4970d27e52c0a764e29bf70ea
Olivier Simard <olivier.simard2@usherbrooke.ca>
2018-01-03 18:11:21 -0500

    Worked on CalcStiffness.jl file to include RBZ calculations. git statusNO CONCLUSIVE TESTS HAVE BEEN DONEgit status

--- CalcStiffness.jl
+++ CalcStiffness.jl
@@ -1,7 +1,10 @@
 using JSON
 using NPZ
 using Glob
+using Memento
 using StiffSquare2.PeriodizeSC
+
+print_with_color(:green, "Extracting info from params.json\n")
 
 paramsfile = "params.json"
 params = JSON.parsefile(paramsfile)
@@ -9,6 +12,14 @@ path_to_files = params["path_to_files"]
 pattern = params["pattern"]
 beta = params["beta"]
 fout_name = params["fout_name"]
+AFM_SC_NOCOEX = params["AFM_SC_NOCOEX"] ## Set to 1 if using AFM_SC function to evaluate NOCOEX data. Set to 0
+                                         # if not.
+
+print_with_color(:green, "Setting parameters for log file\n")
+
+logger = getlogger(current_module())
+push!(logger, DefaultHandler("Julia.log"))
+Memento.config("debug";fmt="[{level}|{name}]:{msg}")
 
 data_file_s = open(readdlm, params["data_loop"][1])
 data_file = open(readdlm, params["data_loop"][2])
@@ -20,27 +31,33 @@ data_file_s_header = data_file_s[1,:]
 #    ind+=1
 #    index=ind
 #end
-
+info(logger, "Reading relevant data from averages*.dat files")
 indmu_s = find(x->x=="mu",data_file_s_header)
 inddop_s = find(x->x=="ave_mu",data_file_s_header)
+indM_s = find(x->x=="ave_M",data_file_s_header)
 
 indmu = find(x->x=="mu",data_file_header)
 inddop = find(x->x=="ave_mu",data_file_header)
+indM = find(x->x=="ave_M",data_file_header)
 
 
 data_file_s_mu = data_file_s[:,indmu_s]
 data_file_s_dop = data_file_s[:,inddop_s]
+data_file_s_M = data_file_s[:,indM_s]
 
 data_file_mu = data_file[:,indmu]
 data_file_dop = data_file[:,inddop]
+data_file_M = data_file[:,indM]
 
 #data_mu = data_file[:,index]
 data_file_s_mu = filter(x->x!="mu",data_file_s_mu)
 data_file_s_dop = filter(x->x!="ave_mu",data_file_s_dop)
+data_file_s_M = filter(x->x!="ave_M",data_file_s_M)
 data_file_s_mu = convert(Array{Float64,1}, data_file_s_mu)
 
 data_file_mu = filter(x->x!="mu",data_file_mu)
 data_file_dop = filter(x->x!="ave_mu",data_file_dop)
+data_file_M = filter(x->x!="ave_M",data_file_M)
 data_file_mu = convert(Array{Float64,1}, data_file_mu)
 
 datap_s = collect(zip(data_file_s_mu,data_file_s_dop))
@@ -64,7 +81,9 @@ datap_h = hcat(data_file_mu,data_file_dop)
 #datap_h_sorted = sort(datap_h,1; alg=MergeSort)
 #println(datap_new_h_sorted)
 super_datap = vcat(datap_h,datap_s_h)
+super_data_M = vcat(data_file_M,data_file_s_M)
 
+info(logger, "Reading SEvec*.dat.npy files obtained from qcm")
 list_of_files = glob(string(path_to_files[2],pattern))
 list_of_files_s = glob(string(path_to_files[1],pattern))
 
@@ -108,14 +127,36 @@ end
 super_list_SEvec_c = vcat(list_SEvec_c, list_SEvec_c_s)
 super_list_t_mu = vcat(list_t_mu,list_t_mu_s)
 
+info(logger, "Entering decision loop regarding if NOCOEX or COEX. Integration over BZ or RBZ")
+m1 = match(r"^(.*?)(?=/)",params["data_loop"][1])
+m2 = match(r"^(.*?)(?=/)",params["data_loop"][2])
+M_mean_field_tol = 1e-5
 list_kIntegral_stiff = []
-for l in 1:length(super_list_t_mu)
-    modelvec = PeriodizeSC.ModelVector(super_list_t_mu[l][1], super_list_t_mu[l][2], super_list_t_mu[l][3], super_list_t_mu[l][4], zvec[1:400], super_list_SEvec_c[l][1, 1:400, :, :])
-    push!(list_kIntegral_stiff,PeriodizeSC.calcintegral_RBZ(modelvec, PeriodizeSC.make_stiffness_kintegrand_test))#Change kintegrand if COEX or NOCOEX
-    println(size(list_kIntegral_stiff[l]))
+if m1.match == "NOCOEX" && m2.match == "NOCOEX" && AFM_SC_NOCOEX == 1
+    notice(logger, "Entered loop with NOCOEX and AFM_SC_NOCOEX = 1")
+    for l in 1:length(super_list_t_mu)
+        modelvec = PeriodizeSC.ModelVector(super_list_t_mu[l][1], super_list_t_mu[l][2], super_list_t_mu[l][3], super_list_t_mu[l][4], zvec[1:400], super_list_SEvec_c[l][1, 1:400, :, :])
+        push!(list_kIntegral_stiff,PeriodizeSC.calcintegral_RBZ(modelvec, PeriodizeSC.make_stiffness_kintegrand_test)) ##Differences if using calcintegral_RBZ (worst) or calcintegral_BZ
+        println(size(list_kIntegral_stiff[l]))
+    end
+else
+    print_with_color(:red, "AFM_SC_NOCOEX option set to 0")
+    notice(logger, "AFM_SC_NOCOEX option set to 0")
+    for l in 1:length(super_list_t_mu)
+        modelvec = PeriodizeSC.ModelVector(super_list_t_mu[l][1], super_list_t_mu[l][2], super_list_t_mu[l][3], super_list_t_mu[l][4], zvec[1:400], super_list_SEvec_c[l][1, 1:400, :, :])
+        if ismatch(r"N[\w]*X",params["data_loop"][1]) && ismatch(r"N[\w]*X",params["data_loop"][2]) || error("Both averages*.dat must come whether from NOCOEX or COEX folders") 
+            push!(list_kIntegral_stiff,PeriodizeSC.calcintegral_BZ(modelvec, PeriodizeSC.make_stiffness_kintegrand_SC))#Change kintegrand if COEX or NOCOEX
+        elseif ismatch(r"^C[\w]*X",params["data_loop"][1]) && ismatch(r"^C[\w]*X",params["data_loop"][2]) || error("Both averages*.dat must come whether from NOCOEX or COEX folders")
+            if abs(super_data_M)>M_mean_field_tol
+                push!(list_kIntegral_stiff,PeriodizeSC.calcintegral_RBZ(modelvec, PeriodizeSC.make_stiffness_kintegrand_test))
+            else 
+                push!(list_kIntegral_stiff,PeriodizeSC.calcintegral_BZ(modelvec, PeriodizeSC.make_stiffness_kintegrand_test))
+            end
+        end        
+        println(size(list_kIntegral_stiff[l]))
+    end
 end
-
-println("Summation in frequency")
+info(logger, "Summation in frequency (beta = 50)")
 stiffness = []
 for j in 1:size(list_kIntegral_stiff)[1]-1
     stiffness_tmp = 0.0
@@ -130,6 +171,8 @@ stiffness*=2 #For the spin projections
 mu_dop_stiff = hcat(super_datap[1:length(stiffness),1],super_datap[1:length(stiffness),2],stiffness)
 println("Stiffness = ", mu_dop_stiff)
 writedlm(fout_name, mu_dop_stiff, "\t\t")
+
+info(logger, "Program exited")
 
 #ll=[]
 #for dop in collect(zip(list_mu,list_dop))
